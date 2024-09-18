67d5f98af0b24da3ffbbdc4c5b3d4b95c0982a48
Beißel <kieran.beissel@btc-it-services.com>
2024-09-18 11:58:19 +0200

    1.02

--- src/main/java/com/btcag/bootcamp/Geometrieberechnung.java
+++ src/main/java/com/btcag/bootcamp/Geometrieberechnung.java
@@ -1,5 +1,4 @@
 package com.btcag.bootcamp;
-import com.sun.source.tree.IfTree;
 
 import java.util.Scanner;
 
@@ -11,14 +10,14 @@ public class Geometrieberechnung {
         Scanner Radius = new Scanner(System.in);
 
         System.out.println("Bitte geben Sie ein, ob sie die Fläche eines Kreises (1) oder eines Rechtecks (2) berechnen möchten: ");
-        int form = Form.nextInt();
-        if (form == 1) {
+        String form = Form.nextLine();
+        if (form .equals("1")) {
             System.out.println("Geben Sie den Radius vom Kreis in cm ein: ");
             double radius = Radius.nextDouble();
             double ErgebnisKreis = ((radius * radius) * 3.14);
             System.out.println("Die Fläche vom Kreis beträgt " + ErgebnisKreis + " cm");
         }
-        else if (form == 2) {
+        else if (form .equals("2")) {
             System.out.println("Geben Sie die erste Seitenlänge vom Rechteck in cm ein: ");
             double seitenlänge1 = Seitenlänge1.nextDouble();
             System.out.println("Geben Sie die zweite Seitenlänge vom Rechteck in cm ein: ");
