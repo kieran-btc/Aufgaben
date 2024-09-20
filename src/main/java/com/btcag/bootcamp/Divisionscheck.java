213c9161cdd1b477aaa68efec9538a7ac652f57e
Beißel <kieran.beissel@btc-it-services.com>
2024-09-20 12:29:22 +0200

    if durch while ersetzt

--- src/main/java/com/btcag/bootcamp/Divisionscheck.java
+++ src/main/java/com/btcag/bootcamp/Divisionscheck.java
@@ -1,4 +1,23 @@
 package com.btcag.bootcamp;
+import java.util.Scanner;
 
 public class Divisionscheck {
+    public static void main(String[] args) {
+        Scanner scan1 = new Scanner(System.in);
+
+        System.out.println("Welchen Divisor willst du prüfen?");
+        int divisor = scan1.nextInt();
+
+        System.out.println("Welche Zahl ist die Grenze?");
+        int grenze = scan1.nextInt();
+
+        int ergebnis = 0;
+        while (ergebnis <= grenze) {
+            while (ergebnis % divisor == 0) {
+                System.out.println(ergebnis);
+                break;
+            }
+            ergebnis = ergebnis + 1;
+        }
+    }
 }
