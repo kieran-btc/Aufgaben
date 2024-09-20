070c3672705ed1b69ad4ba3a200501a2c69bbe3f
Beißel <kieran.beissel@btc-it-services.com>
2024-09-20 11:47:12 +0200

    if zu while geändert

--- src/main/java/com/btcag/bootcamp/Geometrieberechnung.java
+++ src/main/java/com/btcag/bootcamp/Geometrieberechnung.java
@@ -11,23 +11,26 @@ public class Geometrieberechnung {
 
         System.out.println("Bitte geben Sie ein, ob sie die Fläche eines Kreises (1) oder eines Rechtecks (2) berechnen möchten: ");
         String form = Form.nextLine();
-        if (form .equals("1")) {
+        while (form .equals("1")) {
             System.out.println("Geben Sie den Radius vom Kreis in cm ein: ");
             double radius = Radius.nextDouble();
             double ErgebnisKreis = ((radius * radius) * 3.14);
             System.out.println("Die Fläche vom Kreis beträgt " + ErgebnisKreis + " cm");
+            form = form + 2;
         }
-        else if (form .equals("2")) {
+        while (form .equals("2")) {
             System.out.println("Geben Sie die erste Seitenlänge vom Rechteck in cm ein: ");
             double seitenlänge1 = Seitenlänge1.nextDouble();
             System.out.println("Geben Sie die zweite Seitenlänge vom Rechteck in cm ein: ");
             double seitenlänge2 = Seitenlänge2.nextDouble();
             double ErgebnisRechteck = seitenlänge1 * seitenlänge2;
             System.out.println("Die Fläche vom Rechteck beträgt " + ErgebnisRechteck + " cm");
+            form = form + 1;
 
         }
-        else {
+        while (form .equals("3")) {
             System.out.println("Verarschen kann ich mich selber!");
+            break;
         }
     }
 }
